42323cc9f05c2e6fa9b6f2c27fa582394ba14637
Полина Горшкова <polina_gorshkova_0202@mail.ru>
2022-06-22 23:57:13 +0300

    project

---
+++ yatube/posts/views.py
@@ -0,0 +1,21 @@
+# posts/views.py
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
+from .models import Group
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
